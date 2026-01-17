7d5922ce34a895548a362b6a4d2fedb35b22e7dc
Victor Tapia <victortapiaegana@gmai.com>
2026-01-16 22:20:24 -0300

    ADD: Implementar un mensaje de confirmación para transacciones de depósito exitosas

---
+++ js/script-deposit.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+
+  const toastTrigger = document.getElementById('btnConfirmar')
+  const toastLiveExample = document.getElementById('liveToast')
+
+  if (toastTrigger) {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+    toastTrigger.addEventListener('click', () => {
+      toastBootstrap.show()
+    })
+
+    toastTrigger
+
+  }
+
+})
