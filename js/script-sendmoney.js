30c0367cc595682fe9f604da103617b57f4e7e29
Victor Tapia <victortapiaegana@gmai.com>
2026-01-18 14:02:28 -0300

    FIX: Moficico el apecto grafico de la vista transacciones se agreo un tabla con las trasaccinoes realizadas

---
+++ js/script-sendmoney.js
@@ -0,0 +1,22 @@
+const arrayClientes = [
+  {
+    label:'Juan Rojas - ITAU',
+    value:'Juna Rojas',
+  },
+  {
+    label:'Carolina Jorquera - BCI',
+    value:'Carolina Jorquera',
+  },
+  {
+    label:'Eduardo Nuñez - B-CHILE',
+    value:'Eduardo Nuñez',
+  },
+]
+
+document.addEventListener('DOMContentLoaded',() => {
+
+  $('#inputNombre').autocomplete({
+    source: arrayClientes
+  })  
+
+})
