326275fca7ea3d2b63a718df664fc25b0896d0ab
Victor Tapia <victortapiaegana@gmai.com>
2026-01-17 22:12:27 -0300

    ADD: Implementar la lógica de clientes en el script de transacciones

---
+++ js/script-transactions.js
@@ -0,0 +1,20 @@
+const arrayClientes = [
+  {
+    nombre:'Juan Rojas',
+    banco:'ITAU',
+  },
+  {
+    nombre:'Carolina Jorquera',
+    banco:'BCI',
+  },
+  {
+    nombre:'Eduardo Nuñez',
+    banco:'CHILE',
+  },
+]
+
+document.addEventListener('DOMContentLoaded',()=>{
+  // console.table(arrayClientes)
+
+  
+})
